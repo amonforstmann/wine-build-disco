e92b2c608e4a264a1deee1a03500925d667c3a64
Amon Forstmann <amon@anxion.de>
2019-04-17 22:33:58 +0200

    remove wrong comment

--- clean.sh
+++ clean.sh
@@ -5,8 +5,6 @@ set -o pipefail
 # prefer english command output
 export LC_ALL="C"
 
-# script for https://wiki.winehq.org/Building_Wine#Shared_WoW64
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DIR64=${DIR}/64
